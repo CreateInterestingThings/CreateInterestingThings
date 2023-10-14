2e2d758bbc9d63c82a483ca423cac104ff2eee9c
zengxian <zengxian@zhuanzhuan.com>
2023-10-15 01:19:54 +0800

    feat: 🎸 add transform to title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import './index.css'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1>CREATE INTERESTING THINGS</h1>
+      <h1 className="text-5xl main-title">CREATE INTERESTING THINGS</h1>
     </main>
   )
 }
