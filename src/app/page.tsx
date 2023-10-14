ee58b9a5869a8cf6da00f17f16177bd606cdf766
zengxian <zengxian@zhuanzhuan.com>
2023-10-15 01:31:44 +0800

    feat: 🎸 add subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-5xl main-title">CREATE INTERESTING THINGS</h1>
+      <h3 className='sub-title'>COMING SOON...</h3>
     </main>
   )
 }
